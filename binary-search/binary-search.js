88b1b6ee79af89d4fc30259ca4b058c192ed0f10
GNz11 <sshammasova@gmail.com>
2019-10-23 15:50:18 -0400

    completed binary-search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,30 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	if(array.length === 0) {
+		return "array is empty"
+	}
+	if (array.length === 1) {
+			if (target !== array[0]) {
+							return false;
+			} else {
+					return true;
+			}
+	} else {
+			let middle = Math.floor(array.length/2);
+			console.log('middle>>>> ', middle)
+			console.log('array[middle]>>>> ', array[middle])
+			console.log('target>>>> ', target)
+			if (array[middle] === target) {
+					return true;
+			} else if (array[middle] < target) {
+					let slicedArrayRight = array.slice(middle);
+					return binarySearch(slicedArrayRight, target);
+			} else {
+					let slicedArrayLeft = array.slice(0, middle);
+					return binarySearch(slicedArrayLeft, target);
+			}
+	}
 };
 
 /*
--- binary-search/binary-search.spec.js
+++ binary-search/binary-search.spec.js
@@ -5,6 +5,7 @@ const binarySearch = require('./binary-search');
 const test1 = [1,3,4,5,10,15,20,22,23,98,1000];
 const test2 = [-3,-2,0, 9, 10, 11, 12, 15, 17, 18, 19, 20];
 const test3 = [-0.5, 0.03, 2.6, 3, 3.5, 3.777777, 5];
+const test4 = [];
 
 
 describe('Binary Search', () => {
@@ -21,5 +22,9 @@ describe('Binary Search', () => {
 	});
 
 	//(OPTIONAL) WRITE YOUR OWN TESTS HERE. Include >=1 edge case :)
+
+	it('returns false if array is empty', () => {
+		expect(binarySearch(test4, 17)).to.equal("array is empty");
+	});
 
 });
