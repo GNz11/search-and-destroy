af9824896d737147a76bd84f22314ac2479ca80d
GNz11 <sshammasova@gmail.com>
2019-10-24 11:00:35 -0400

    jumps solution

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,6 +2,22 @@
 
 // Complete this algo
 const minJumps = arr => {
+// min number of jumps to get to each index in the array
+
+const minJumps = arr => {
+	let jumps = arr.map(el => Infinity)
+	jumps[0] = 0;
+
+	for(let i = 1; i < jumps.length; i++){
+		for(let j = 0; j <= i; j++){
+			if(j + arr[j] >= i) {
+				jumps[i] = Math.min(jumps[i], jumps[j] + 1)
+			}
+		}
+	}
+	return jumps[arr.length - 1];
+};
+
 
 };
 
